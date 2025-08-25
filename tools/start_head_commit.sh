a93a433132f1e8bd592c0c887b1906627eb5ebce
running-terminal-command-blog <running-terminal-command-blog@test.com>
2025-08-25 23:59:49 +0900

    リポジトリのコミットでheadに該当するファイルを自動でテストするツール追加

---
+++ tools/start_head_commit.sh
@@ -0,0 +1,34 @@
+sleep_time=2
+
+
+git checkout head
+echo "カレントディレクトリの中身を表示します"
+ls
+echo
+sleep $sleep_time
+
+echo "makeコマンドを実行します"
+make
+sleep $sleep_time
+
+echo "カレントディレクトリの中身を表示します"
+ls
+echo
+sleep $sleep_time
+
+echo "ビルドしたファイルを実行します"
+./a.out
+echo
+sleep $sleep_time
+
+echo "生成されたオブジェクトファイルをmakeコマンド経由で削除します"
+make rm_object
+echo
+sleep $sleep_time
+
+echo "カレントディレクトリの中身を表示します"
+ls
+echo
+sleep $sleep_time
+
+echo "以上で終了です"
