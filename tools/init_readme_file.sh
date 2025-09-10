fb26d8f233f1d0b1cf066445062151459109dff0
running-terminal-command-blog <running-terminal-command-blog@test.com>
2025-09-10 23:27:59 +0900

    READMEのテンプレートを作成するファイルを追加

---
+++ tools/init_readme_file.sh
@@ -0,0 +1,114 @@
+# ./init_readme_file.sh
+
+
+cd ..
+cat<<EOF > template_README.md
+# makefile running-terminal-command-blog
+
+## introduction
+このリポジトリはmakeコマンドを実行して、makeコマンドとmakefileの理解を深めようとする目的で作成しました
+
+<br />
+
+## 使用するコマンド
+   
+
+<br />
+
+## クイックスタート
+以下のコマンドを実行してリポジトリからダウンロード後、ディレクトリを移動しmakeで実行できます
+### linux/macos
+   ```bash
+   
+   ```
+
+<br />
+
+### windows
+   ```bash
+   
+   ```
+
+<br />
+
+## 実行手順
+
+### linux
+<details>
+<summary>クリックして詳細を表示</summary>
+
+#### 事前確認
+以下のコマンドをターミナルに打ち込んでcommand not foundが出なければokです
+   ```bash
+   
+   ```
+   
+#### preinstall
+command not foundが出たコマンドを以下のコマンドでインストールしてください
+   ```bash
+   
+   ```
+
+#### コマンド
+   以下のコマンドを実行することで詳細のコマンド群を自動で実行してくれます
+   ```bash
+   
+   ```
+
+   <details>
+      <summary>make linuxコマンドの詳細</summary>
+
+   </details>
+</details>
+
+<br />
+
+### macos
+<details>
+<summary>クリックして詳細を表示</summary>
+
+#### 事前準備
+以下のコマンドをターミナルに打ち込んでcommand not foundが出なければokです
+   ```bash
+   
+   ```
+
+#### preinstall
+command not foundが出たコマンドを以下のコマンドでインストールしてください
+   ```bash
+   
+   ```
+
+#### コマンド
+以下のコマンドを実行することで詳細のコマンド群を自動で実行してくれます
+   ```bash
+   
+   ```
+
+   <details>
+      <summary>make macosコマンドの詳細</summary>
+
+   </details>
+</details>
+
+<br />
+
+### windows
+<details>
+<summary>クリックして詳細を表示</summary>
+
+#### 事前準備
+
+#### preinstall
+
+#### コマンド
+</details>
+
+<br />
+
+## 補足要素
+
+
+
+EOF
+cd tools
