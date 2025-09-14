6dc17f524be37453b4372a861c9ff2e031262826
running-terminal-command-blog <running-terminal-command-blog@test.com>
2025-09-14 18:11:32 +0900

    windowsで使用するコマンド群を実行するファイルを追加

---
+++ tools/for_workflow/for_windows10.sh
@@ -0,0 +1,39 @@
+# ./tools/for_workflow/for_windows10.sh
+
+
+sleep_time=2
+
+
+git checkout HEAD
+echo "カレントディレクトリの中身を表示します"
+ls
+echo
+sleep $sleep_time
+
+echo "mingw32-makeコマンドを実行します"
+mingw32-make
+sleep $sleep_time
+
+echo "カレントディレクトリの中身を表示します"
+ls
+echo
+sleep $sleep_time
+
+echo "ビルドしたファイルを実行します"
+./a.out
+echo
+sleep $sleep_time
+
+echo "生成されたオブジェクトファイルをmingw32-makeコマンド経由で削除します"
+mingw32-make rm_generated
+echo
+sleep $sleep_time
+
+echo "カレントディレクトリの中身を表示します"
+ls
+echo
+sleep $sleep_time
+
+echo "以上で終了です"
+
+
